dbb116b77f02c93ce21a8e21c23da5af15bab6a3
Fatimah Ulwiyatul Badriyah <faatimahbadriyah@gmail.com>
2022-10-11 13:34:10 +0700

    initial spring boot project

---
+++ src/main/java/com/subrutin/catalogue/BookCatalogueV2Application.java
@@ -0,0 +1,13 @@
+package com.subrutin.catalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookCatalogueV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookCatalogueV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/subrutin/catalogue/BookCatalogueV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subrutin.catalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookCatalogueV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
