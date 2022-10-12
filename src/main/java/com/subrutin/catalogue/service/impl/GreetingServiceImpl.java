7b61e0261e59343778b317fee4f9c24b7d048dd6
Fatimah Ulwiyatul Badriyah <faatimahbadriyah@gmail.com>
2022-10-12 16:12:03 +0700

    add spring configuration processor (tidy up properties)

--- src/main/java/com/subrutin/catalogue/service/impl/GreetingServiceImpl.java
+++ src/main/java/com/subrutin/catalogue/service/impl/GreetingServiceImpl.java
@@ -3,28 +3,26 @@ package com.subrutin.catalogue.service.impl;
 
 import java.util.TimeZone;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.subrutin.catalogue.config.ApplicationProperties;
 import com.subrutin.catalogue.service.GreetingService;
 
 @Service
 public class GreetingServiceImpl implements GreetingService{
 	
-	@Value("${welcome.text}")
-	private String welcomeText;
-	
-	@Value("${timezone}")
-	private String timeZone;
+	private ApplicationProperties appProperties;
 
-	@Value("${currency}")
-	private String currency;
+	public GreetingServiceImpl(ApplicationProperties appProperties) {
+		super();
+		this.appProperties = appProperties;
+	}
 
 	@Override
 	public String sayGreeting() {
-		TimeZone timezone = TimeZone.getTimeZone(timeZone);
-		return welcomeText+", our timezone :"+timezone.getDisplayName()+
-				", our currency: "+currency;
+		TimeZone timezone = TimeZone.getTimeZone(appProperties.getTimezone());
+		return appProperties.getWelcomeText()+", our timezone :"+timezone.getDisplayName()+
+				", our currency: "+appProperties.getCurrency();
 	}
 
 }
