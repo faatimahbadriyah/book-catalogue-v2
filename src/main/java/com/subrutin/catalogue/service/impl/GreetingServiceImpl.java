a3a1770fe4ec4d22806edb2ba0c57e9f4726bcde
Fatimah Ulwiyatul Badriyah <faatimahbadriyah@gmail.com>
2022-10-12 15:47:01 +0700

    add several properties

--- src/main/java/com/subrutin/catalogue/service/impl/GreetingServiceImpl.java
+++ src/main/java/com/subrutin/catalogue/service/impl/GreetingServiceImpl.java
@@ -1,6 +1,8 @@
 package com.subrutin.catalogue.service.impl;
 
 
+import java.util.TimeZone;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +13,18 @@ public class GreetingServiceImpl implements GreetingService{
 	
 	@Value("${welcome.text}")
 	private String welcomeText;
+	
+	@Value("${timezone}")
+	private String timeZone;
+
+	@Value("${currency}")
+	private String currency;
 
 	@Override
 	public String sayGreeting() {
-		return welcomeText;
+		TimeZone timezone = TimeZone.getTimeZone(timeZone);
+		return welcomeText+", our timezone :"+timezone.getDisplayName()+
+				", our currency: "+currency;
 	}
 
 }
