6896a22eb8c358f0f54063fed1c2967302ee7788
Fatimah Ulwiyatul Badriyah <faatimahbadriyah@gmail.com>
2022-10-12 16:50:23 +0700

    add properties on evironment variable system & environment IDE

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Syntax
 ## run via cli
 > mvn spring-boot:run
+
 ## ways to override properties
 1. via src/main/resource/application.properties
 2. via cli 
@@ -9,3 +10,7 @@
 - Spring Boot->Override Properties->server.port=8082
 - Arguments->Program arguments->--server.port=8083
 - Arguments->VM arguments->-Dserver.port=8084
+
+## ways to set up properties
+1. add from environment variables OS (need restart)
+2. add from Run Configurations -> Environtment -> add
--- src/main/java/com/subrutin/catalogue/service/impl/GreetingServiceImpl.java
+++ src/main/java/com/subrutin/catalogue/service/impl/GreetingServiceImpl.java
@@ -6,20 +6,25 @@ import java.util.TimeZone;
 import org.springframework.stereotype.Service;
 
 import com.subrutin.catalogue.config.ApplicationProperties;
+import com.subrutin.catalogue.config.CloudProperties;
 import com.subrutin.catalogue.service.GreetingService;
 
 @Service
 public class GreetingServiceImpl implements GreetingService{
 	
 	private ApplicationProperties appProperties;
+	
+	private CloudProperties cloudProperties;
 
-	public GreetingServiceImpl(ApplicationProperties appProperties) {
+	public GreetingServiceImpl(ApplicationProperties appProperties, CloudProperties cloudProperties) {
 		super();
 		this.appProperties = appProperties;
+		this.cloudProperties = cloudProperties;
 	}
 
 	@Override
 	public String sayGreeting() {
+		System.out.println(cloudProperties.getApiKey());
 		TimeZone timezone = TimeZone.getTimeZone(appProperties.getTimezone());
 		return appProperties.getWelcomeText()+", our timezone :"+timezone.getDisplayName()+
 				", our currency: "+appProperties.getCurrency();
