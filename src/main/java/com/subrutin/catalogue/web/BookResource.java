0bf7bb0e52ee0234140bca1962bde9cda31b1276
Fatimah Ulwiyatul Badriyah <faatimahbadriyah@gmail.com>
2022-10-14 14:21:18 +0700

    change manual logger to lombok (to avoid verbose app)

--- src/main/java/com/subrutin/catalogue/web/BookResource.java
+++ src/main/java/com/subrutin/catalogue/web/BookResource.java
@@ -1,7 +1,5 @@
 package com.subrutin.catalogue.web;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,23 +9,19 @@ import com.subrutin.catalogue.dto.BookDetailDTO;
 import com.subrutin.catalogue.service.BookService;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
+@AllArgsConstructor
 @RestController
 public class BookResource {
 
-	Logger log = LoggerFactory.getLogger(HelloResources.class);
-
 	private final BookService bookService;
-	
-	public BookResource(BookService bookService) {
-		super();
-		this.bookService = bookService;
-	}
 
 	@GetMapping("/book/{bookId}")
 	public BookDetailDTO findBookDetail(@PathVariable("bookId") Long id) {
 		StopWatch stopWatch = new StopWatch();
-		log.info("start find book detail" + id);
+		log.info("start find book detail " + id);
 		stopWatch.start();
 		BookDetailDTO result = bookService.findBookDetailById(id);
 		log.info("finish find book detail. execution time = {}", stopWatch.getTotalTimeMillis());
