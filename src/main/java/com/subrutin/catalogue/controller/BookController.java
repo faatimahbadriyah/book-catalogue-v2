e5f9b4c7022fbe3a079e92ce16b7a7356830cc36
Fatimah Ulwiyatul Badriyah <faatimahbadriyah@gmail.com>
2022-10-14 16:40:23 +0700

    create form new book

--- src/main/java/com/subrutin/catalogue/controller/BookController.java
+++ src/main/java/com/subrutin/catalogue/controller/BookController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.subrutin.catalogue.dto.BookDetailDTO;
 import com.subrutin.catalogue.service.BookService;
@@ -13,15 +14,21 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @Controller
+@RequestMapping("/book")
 public class BookController {
 	
 	private final BookService bookService;
 	
-	@GetMapping("/books")
+	@GetMapping("/list")
 	public String findBookList(Model model) {
 		List<BookDetailDTO> books = bookService.findBookListDetail();
 		model.addAttribute("books", books);
 		return "book/list";
+	}
+	
+	@GetMapping("/new")
+	public String loadBookForm(Model model) {
+		return "book/new";
 	}
 
 }
