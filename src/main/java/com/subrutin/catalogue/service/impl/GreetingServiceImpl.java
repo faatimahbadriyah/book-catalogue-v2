0b40f68fdc74152266d059c7d375241725e1d7dc
Fatimah Ulwiyatul Badriyah <faatimahbadriyah@gmail.com>
2022-10-14 11:03:36 +0700

    add and set up level log specific package

--- src/main/java/com/subrutin/catalogue/service/impl/GreetingServiceImpl.java
+++ src/main/java/com/subrutin/catalogue/service/impl/GreetingServiceImpl.java
@@ -3,6 +3,8 @@ package com.subrutin.catalogue.service.impl;
 
 import java.util.TimeZone;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.subrutin.catalogue.config.ApplicationProperties;
@@ -11,6 +13,8 @@ import com.subrutin.catalogue.service.GreetingService;
 
 @Service
 public class GreetingServiceImpl implements GreetingService{
+	
+	Logger log = LoggerFactory.getLogger(GreetingServiceImpl.class);
 	
 	private ApplicationProperties appProperties;
 	
@@ -24,6 +28,12 @@ public class GreetingServiceImpl implements GreetingService{
 
 	@Override
 	public String sayGreeting() {
+		//level log
+		log.trace("This is log TRACE greeting service");
+		log.debug("This is log DEBUG greeting service");
+		log.info("This is log INFO greeting service");
+		log.warn("This is log WARN greeting service");
+		log.error("This is log ERROR greeting service");
 		System.out.println(cloudProperties.getApiKey());
 		TimeZone timezone = TimeZone.getTimeZone(appProperties.getTimezone());
 		return appProperties.getWelcomeText()+", our timezone :"+timezone.getDisplayName()+
