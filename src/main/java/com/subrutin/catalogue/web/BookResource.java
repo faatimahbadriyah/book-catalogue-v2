b69a8a7f609b58e5955bc76d939887b7040bd457
Fatimah Ulwiyatul Badriyah <faatimahbadriyah@gmail.com>
2022-10-14 14:12:13 +0700

    add lombok logger

--- src/main/java/com/subrutin/catalogue/web/BookResource.java
+++ src/main/java/com/subrutin/catalogue/web/BookResource.java
@@ -1,5 +1,8 @@
 package com.subrutin.catalogue.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,11 +16,18 @@ import lombok.AllArgsConstructor;
 @RestController
 public class BookResource {
 
+	Logger log = LoggerFactory.getLogger(HelloResources.class);
+
 	private final BookService bookService;
 	
 	@GetMapping("/book/{bookId}")
 	public BookDetailDTO findBookDetail(@PathVariable("bookId") Long id) {
-		return bookService.findBookDetailById(id);
+		StopWatch stopWatch = new StopWatch();
+		log.info("start find book detail" + id);
+		stopWatch.start();
+		BookDetailDTO result = bookService.findBookDetailById(id);
+		log.info("finish find book detail. execution time = {}", stopWatch.getTotalTimeMillis());
+		return result;
 	}
 	
 }
