bcc3b3f95a7b68b96833aaf7b2642b36cfecf85d
Fatimah Ulwiyatul Badriyah <faatimahbadriyah@gmail.com>
2022-10-20 08:44:14 +0700

    add validation new book

--- src/main/java/com/subrutin/catalogue/dto/BookCreateDTO.java
+++ src/main/java/com/subrutin/catalogue/dto/BookCreateDTO.java
@@ -1,12 +1,16 @@
 package com.subrutin.catalogue.dto;
 
+import javax.validation.constraints.NotBlank;
+
 import lombok.Data;
 
 @Data
 public class BookCreateDTO {
 	
+	@NotBlank
 	private String bookTitle;
 	
+	@NotBlank
 	private String authorName;
 	
 	private String description;
