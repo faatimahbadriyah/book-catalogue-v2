b928fc88a938c7ab078aeb730767475d8591c1e1
Fatimah Ulwiyatul Badriyah <faatimahbadriyah@gmail.com>
2022-10-26 09:05:01 +0700

    add snake case naming on list book

--- src/main/java/com/subrutin/catalogue/dto/BookDetailDTO.java
+++ src/main/java/com/subrutin/catalogue/dto/BookDetailDTO.java
@@ -2,9 +2,13 @@ package com.subrutin.catalogue.dto;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import lombok.Data;
 
 @Data
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class BookDetailDTO implements Serializable{
 	
 	private static final long serialVersionUID = -7889938648939242355L;
