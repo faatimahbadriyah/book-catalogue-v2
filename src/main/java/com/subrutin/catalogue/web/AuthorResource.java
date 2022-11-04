a7957d86ca1a040f7da6c54d1c163345d0a96b3d
Fatimah Ulwiyatul Badriyah <faatimahbadriyah@gmail.com>
2022-11-04 16:03:05 +0700

    add endpoint delete data

--- src/main/java/com/subrutin/catalogue/web/AuthorResource.java
+++ src/main/java/com/subrutin/catalogue/web/AuthorResource.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,6 +41,12 @@ public class AuthorResource {
 	@PutMapping("/author/{authorId}")
 	public ResponseEntity<Void> updateAuthor(@PathVariable Long authorId, @RequestBody AuthorUpdateRequestDTO dto){
 		authorService.updateAuthor(authorId, dto);
+		return ResponseEntity.ok().build();
+	}
+	
+	@DeleteMapping("/author/{authorId}")
+	public ResponseEntity<Void> deleteAuthor(@PathVariable Long authorId){
+		authorService.deleteAuthor(authorId);
 		return ResponseEntity.ok().build();
 	}
 	
