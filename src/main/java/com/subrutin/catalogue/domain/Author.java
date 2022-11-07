78f2cc22755d2832e6a6ba34c5cffe93567b6e32
Fatimah Ulwiyatul Badriyah <faatimahbadriyah@gmail.com>
2022-11-07 16:53:32 +0700

    implement uuid (author)

--- src/main/java/com/subrutin/catalogue/domain/Author.java
+++ src/main/java/com/subrutin/catalogue/domain/Author.java
@@ -2,12 +2,14 @@ package com.subrutin.catalogue.domain;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.UUID;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Index;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
@@ -23,7 +25,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "author")
+@Table(name = "author", indexes = {
+		@Index(name = "uk_secure_id", columnList = "secure_id")
+})
 @DynamicUpdate
 @Where(clause = "deleted=false")
 @SQLDelete(sql = "UPDATE author SET deleted = true WHERE id = ?")
@@ -38,6 +42,9 @@ public class Author implements Serializable{
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "author_generator")
 	@SequenceGenerator(name = "author_generator", sequenceName = "author_id_seq")
 	private Long id;
+	
+	@Column(name = "secure_id", nullable = false, unique = false)
+	private String secureId = UUID.randomUUID().toString();
 	
 	@Column(name = "author_name", nullable = false, columnDefinition = "varchar(300)")
 	private String name;
