b11822f01c6f4e6072030e4db1fad351a0c05ea7
Fatimah Ulwiyatul Badriyah <faatimahbadriyah@gmail.com>
2022-11-10 16:56:34 +0700

    add versioning author resource

--- src/main/java/com/subrutin/catalogue/web/AuthorResource.java
+++ src/main/java/com/subrutin/catalogue/web/AuthorResource.java
@@ -27,24 +27,24 @@ public class AuthorResource {
 	
 	private final AuthorService authorService;
 
-	@GetMapping("/author/{id}/detail")
+	@GetMapping("/v1/author/{id}/detail")
 	public ResponseEntity<AuthorResponseDTO> findAuthorById(@PathVariable String id){
 		return ResponseEntity.ok().body(authorService.findAuthorById(id));
 	}
 	
-	@PostMapping("/author")
+	@PostMapping("/v1/author")
 	public ResponseEntity<Void> createNewAuthor(@RequestBody @Valid List<AuthorCreateRequestDTO> dto){
 		authorService.createNewAuthor(dto);
 		return ResponseEntity.created(URI.create("/author")).build();
 	}
 	
-	@PutMapping("/author/{authorId}")
+	@PutMapping("/v1/author/{authorId}")
 	public ResponseEntity<Void> updateAuthor(@PathVariable String authorId, @RequestBody AuthorUpdateRequestDTO dto){
 		authorService.updateAuthor(authorId, dto);
 		return ResponseEntity.ok().build();
 	}
 	
-	@DeleteMapping("/author/{authorId}")
+	@DeleteMapping("/v1/author/{authorId}")
 	public ResponseEntity<Void> deleteAuthor(@PathVariable String authorId){
 		authorService.deleteAuthor(authorId);
 		return ResponseEntity.ok().build();
