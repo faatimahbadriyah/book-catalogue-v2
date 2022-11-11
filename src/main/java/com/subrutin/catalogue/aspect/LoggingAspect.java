4fb00d90e172a49b3d1659700a30ee68c29b9c7a
Fatimah Ulwiyatul Badriyah <faatimahbadriyah@gmail.com>
2022-11-11 16:58:53 +0700

    implement Spring AOP

---
+++ src/main/java/com/subrutin/catalogue/aspect/LoggingAspect.java
@@ -0,0 +1,25 @@
+package com.subrutin.catalogue.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+@Aspect
+public class LoggingAspect {
+	
+	@Pointcut("execution(* com.subrutin.catalogue.web.BookResource.findBookDetail(..))")
+	private void restAPI() {}
+	
+	@Before("restAPI()")
+	public void beforeExecutedLog() {
+		
+		log.info("this is log from aspect");
+		
+	}
+
+}
