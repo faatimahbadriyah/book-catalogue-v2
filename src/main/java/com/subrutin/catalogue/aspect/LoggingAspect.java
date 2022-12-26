ac83492e944487a701b9d348acf738627440e9c2
Fatimah Ulwiyatul Badriyah <faatimahbadriyah@gmail.com>
2022-12-26 17:25:03 +0700

    implement advise around into logging feature

--- src/main/java/com/subrutin/catalogue/aspect/LoggingAspect.java
+++ src/main/java/com/subrutin/catalogue/aspect/LoggingAspect.java
@@ -1,12 +1,15 @@
 package com.subrutin.catalogue.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -61,6 +64,23 @@ public class LoggingAspect {
 	@AfterThrowing("annotationPointcutExample()")
 	public void afterThrowingExecutedLog() {
 		log.info("this is log from aspect annotation after throwing method executed");
+	}
+
+	@Around("restAPI()")
+	public Object processingTimeLogging(ProceedingJoinPoint joinPoint) throws Throwable {
+
+		StopWatch stopWatch = new StopWatch();
+		try {
+			log.info("start {}.{} ", joinPoint.getTarget().getClass().getName(), joinPoint.getSignature().getName());
+			stopWatch.start();
+			return joinPoint.proceed();
+		} finally {
+			stopWatch.stop();
+			log.info("finish {}.{} execution time = {} ", joinPoint.getTarget().getClass().getName(),
+					joinPoint.getSignature().getName(), stopWatch.getTotalTimeMillis());
+
+		}
+
 	}
 
 }
