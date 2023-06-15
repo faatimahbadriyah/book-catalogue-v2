78b3a0a174caff5cf544cc97f820fa3bc7bf10ac
Fatimah Ulwiyatul Badriyah <faatimahbadriyah@gmail.com>
2023-06-15 11:26:03 +0700

    add domain AppUser, Role & create their relations

---
+++ src/main/java/com/subrutin/catalogue/domain/AppUser.java
@@ -0,0 +1,79 @@
+package com.subrutin.catalogue.domain;
+
+import java.util.Collection;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "app_user")
+public class AppUser extends AbstractBaseEntity implements UserDetails{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2579191400372699530L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "username", nullable = false)
+	private String username;
+	
+	@Column(name = "password", nullable = false)
+	private String password;
+	
+	@ManyToMany
+	@JoinTable(name = "user_role", joinColumns = {
+			@JoinColumn(name="user_id", referencedColumnName = "id")
+	}, inverseJoinColumns = {
+			@JoinColumn(name="role_id", referencedColumnName = "id")
+	})
+	private List<Role> roles;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/main/java/com/subrutin/catalogue/domain/Role.java
@@ -0,0 +1,38 @@
+package com.subrutin.catalogue.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "role")
+public class Role implements GrantedAuthority{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6500748939044557461L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "name", nullable = false)
+	private String name;
+
+	@Override
+	public String getAuthority() {
+		return "ROLE_" + name;
+	}
+	
+	
+
+}
