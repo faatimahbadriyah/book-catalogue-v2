6a772f2574835475d2211b446ae22982fde6bcb3
Fatimah Ulwiyatul Badriyah <faatimahbadriyah@gmail.com>
2023-06-20 14:49:47 +0700

    add fetchtype eager

--- src/main/java/com/subrutin/catalogue/domain/AppUser.java
+++ src/main/java/com/subrutin/catalogue/domain/AppUser.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -39,7 +40,7 @@ public class AppUser extends AbstractBaseEntity implements UserDetails {
 	@Column(name = "password", nullable = false)
 	private String password;
 
-	@ManyToMany
+	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(name = "user_role", joinColumns = {
 			@JoinColumn(name = "user_id", referencedColumnName = "id") }, inverseJoinColumns = {
 					@JoinColumn(name = "role_id", referencedColumnName = "id") })
